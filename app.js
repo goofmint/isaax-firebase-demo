474e49427f67ba4e5b22da59c0e37333d15b01ad
Atsushi <atsushi@moongift.jp>
2018-11-26 10:27:45 +0900

    Execute temp

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const {exec} = require('child_process');
 const {promisify} = require('util');
 const deviceId = 'pi';
 
-const projectId = 'isaax-demo';
+const projectId = 'server/saving-data/fireblog';
 const collectionId = 'cpu';
 const serviceAccount = require("./isaax-demo-firebase-adminsdk-p27d1-914f648c7d.json");
 
@@ -13,12 +13,12 @@ admin.initializeApp({
 });
 const db = admin.database();
 const ref = db.ref(collectionId);
-
 setInterval(async () => {
   try {
     const {stdout, stderr} = await promisify(exec)('vcgencmd measure_temp');
     const temp = parseFloat(stdout.replace(/temp=([0-9\.]*)'C/, '$1'));
-    const res = await ref.push({
+    const obj = ref.child("temp");
+    obj.set({
       device: deviceId,
       value: temp
     });

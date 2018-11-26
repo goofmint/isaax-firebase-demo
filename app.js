a99353f6cb15a1b4ed9beb8f04bc22526db6e8ee
Atsushi <atsushi@moongift.jp>
2018-11-26 10:02:08 +0900

    Execute temp

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const admin = require("firebase-admin");
+const {exec} = require('child_process');
+const {promisify} = require('util');
+
 const projectId = 'isaax-demo';
 const collectionId = 'cpu';
-
-const admin = require("firebase-admin");
-
 const serviceAccount = require("./isaax-demo-firebase-adminsdk-p27d1-914f648c7d.json");
 
 admin.initializeApp({
@@ -10,3 +11,12 @@ admin.initializeApp({
   databaseURL: "https://isaax-demo.firebaseio.com"
 });
 
+
+setInterval(async () => {
+  try {
+    const result = await promisify(exec)('vcgencmd measure_temp');
+    console.log('result', result);
+  } catch (e) {
+    console.log(e)
+  }
+}, 2000);
